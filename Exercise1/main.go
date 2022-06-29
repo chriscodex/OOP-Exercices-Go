04fd0ef3cb551135a3d55e7299d603e27dc468e2
Christian <3003html@gmail.com>
2022-06-28 19:16:19 -0500

    Finished

--- Exercise1/main.go
+++ Exercise1/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// Exercise 1 from https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-programacion-orientado-a-objetos-java/
+
 type Cuenta struct {
 	titular  string
 	cantidad float64
@@ -44,6 +46,22 @@ func (c *Cuenta) toString() string {
 	return fmt.Sprintf("La cuenta de %s tiene %v euros", c.titular, c.cantidad)
 }
 
+// Special methods
+func (c *Cuenta) ingresar(cant float64) {
+	if cant > 0 {
+		c.cantidad += cant
+	}
+}
+
+func (c *Cuenta) retirar(cant float64) {
+	if c.cantidad-cant < 0 {
+		fmt.Println("Error, Está intentando hacer un retiro mayor a su saldo")
+	} else {
+		c.cantidad -= cant
+		fmt.Println("Saldo retirado con éxito")
+	}
+}
+
 func main() {
 	miCuenta := NewCuenta("Christian", 50.0)
 	fmt.Println(*miCuenta)
@@ -51,4 +69,8 @@ func main() {
 	miCuenta.setTitular("Peter")
 	miCuenta.setCantidad(1.25)
 	fmt.Println(miCuenta.toString())
+	miCuenta.ingresar(30)
+	fmt.Println(miCuenta.toString())
+	miCuenta.retirar(18)
+	fmt.Println(miCuenta.getCantidad())
 }
