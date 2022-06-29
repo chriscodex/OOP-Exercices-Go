bef6d75ea1fefc7d1e41ee6d066def5d43686b12
Christian <3003html@gmail.com>
2022-06-28 20:07:03 -0500

    e2: Constructor 2 created

---
+++ Exercise2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type persona struct {
+	dni    string
+	nombre string
+	edad   int
+	sexo   string
+	peso   float32
+	altura float32
+}
+
+// Constructor por defecto
+func NewPersona1(dni string) *persona {
+	return &persona{
+		dni:  dni,
+		sexo: "H",
+	}
+}
+
+// Constructor 2
+func NewPersona2(dni string, nombre string, edad int, sexo string) *persona {
+	return &persona{
+		dni:    dni,
+		nombre: nombre,
+		edad:   edad,
+		sexo:   sexo,
+	}
+}
+
+func main() {
+	pers1 := NewPersona1("1561")
+	pers2 := NewPersona2("74062106", "Christian", 24, "F")
+	fmt.Println(*pers2, *pers1)
+}
