c7e776637ac64f7ca4842a98c1697ec5e01815d4
Christian <3003html@gmail.com>
2022-06-28 20:19:49 -0500

    esMayordeEdad function implemented

--- Exercise2/main.go
+++ Exercise2/main.go
@@ -57,10 +57,18 @@ func (p *persona) calcularIMC() int {
 	}
 }
 
+func (p *persona) esMayorDeEdad() bool {
+	if p.edad >= 18 {
+		return true
+	}
+	return false
+}
+
 func main() {
 	pers1 := NewPersona1("1561")
 	pers2 := NewPersona2("74062106", "Christian", 24, "F")
 	pers3 := NewPersona3("1231244", "Eduardo", 24, "M", 26, 1)
 	fmt.Println(pers3.calcularIMC())
+	fmt.Println(pers3.esMayorDeEdad())
 	fmt.Println(*pers2, *pers1)
 }
