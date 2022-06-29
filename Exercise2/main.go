06704829a3689e002b75b5f7f974d3978b96296d
Christian <3003html@gmail.com>
2022-06-28 20:09:05 -0500

    e2: constructor 3 created

--- Exercise2/main.go
+++ Exercise2/main.go
@@ -29,6 +29,18 @@ func NewPersona2(dni string, nombre string, edad int, sexo string) *persona {
 	}
 }
 
+// Constructor 3
+func NewPersona3(dni string, nombre string, edad int, sexo string, peso float32, altura float32) *persona {
+	return &persona{
+		dni:    dni,
+		nombre: nombre,
+		edad:   edad,
+		sexo:   sexo,
+		peso:   peso,
+		altura: altura,
+	}
+}
+
 func main() {
 	pers1 := NewPersona1("1561")
 	pers2 := NewPersona2("74062106", "Christian", 24, "F")
