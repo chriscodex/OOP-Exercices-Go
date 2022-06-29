1ef6885bb68e5e65c4bf4dab60c8fee7c4afaef5
Christian <3003html@gmail.com>
2022-06-28 19:02:39 -0500

    e1: setters and function toString created

--- Exercise1/main.go
+++ Exercise1/main.go
@@ -30,8 +30,25 @@ func (c *Cuenta) getCantidad() float64 {
 	return c.cantidad
 }
 
+// Setters
+func (c *Cuenta) setTitular(t string) {
+	c.titular = t
+}
+
+func (c *Cuenta) setCantidad(cant float64) {
+	c.cantidad = cant
+}
+
+// Function toString
+func (c *Cuenta) toString() string {
+	return fmt.Sprintf("La cuenta de %s tiene %v euros", c.titular, c.cantidad)
+}
+
 func main() {
 	miCuenta := NewCuenta("Christian", 50.0)
 	fmt.Println(*miCuenta)
 	fmt.Println(miCuenta.getTitular())
+	miCuenta.setTitular("Peter")
+	miCuenta.setCantidad(1.25)
+	fmt.Println(miCuenta.toString())
 }
