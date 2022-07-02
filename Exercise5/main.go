f4ec34cb95b28adeac87a2adbaefa248fc9c4992
Christian <3003html@gmail.com>
2022-07-02 17:43:01 -0500

    Function main modified, arrays and for loops created

--- Exercise5/main.go
+++ Exercise5/main.go
@@ -6,11 +6,32 @@ import (
 )
 
 func main() {
-	se := st.NewSerie3("The boys", 3, true, "action", "some1")
-	vj := st.NewVideojuego()
-	vj.Devolver()
-	se.Devolver()
-	se.IsEntregado()
-	fmt.Println(vj.Entregado)
-	fmt.Println(se.Entregado)
+	arraySeries := [5]st.Serie{
+		st.NewSerie3("The boys", 3, true, "action", "some1"),
+		st.NewSerie(),
+		st.NewSerie(),
+		st.NewSerie(),
+		st.NewSerie(),
+	}
+	arrayVideojuegos := [5]st.Videojuego{
+		st.NewVideojuego(),
+		st.NewVideoJuego2("Mario", 36),
+		st.NewVideojuego(),
+		st.NewVideojuego(),
+		st.NewVideojuego(),
+	}
+	arraySeries[0].Entregar()
+	arrayVideojuegos[1].Entregar()
+	fmt.Println("Series")
+	for i, e := range arraySeries {
+		if arraySeries[i].Entregado {
+			fmt.Println(e)
+		}
+	}
+	fmt.Println("Videojuegos")
+	for i, e := range arrayVideojuegos {
+		if arrayVideojuegos[i].Entregado {
+			fmt.Println(e)
+		}
+	}
 }
