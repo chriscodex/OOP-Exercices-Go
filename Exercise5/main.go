095750da6169cf3e23e7a4bd7ac065913632b03f
Christian <3003html@gmail.com>
2022-07-02 18:15:40 -0500

    e5: function main completed

--- Exercise5/main.go
+++ Exercise5/main.go
@@ -7,30 +7,58 @@ import (
 
 func main() {
 	arraySeries := [5]st.Serie{
-		st.NewSerie3("The boys", 3, true, "action", "some1"),
-		st.NewSerie(),
+		st.NewSerie3("The boys", 12, true, "action", "unknown"),
+		st.NewSerie3("Breaking Bad", 12, true, "action", "unknown"),
 		st.NewSerie(),
 		st.NewSerie(),
 		st.NewSerie(),
 	}
 	arrayVideojuegos := [5]st.Videojuego{
 		st.NewVideojuego(),
-		st.NewVideoJuego2("Mario", 36),
-		st.NewVideojuego(),
+		st.NewVideoJuego2("Mario Bros", 36),
+		st.NewVideoJuego2("WC3", 36),
 		st.NewVideojuego(),
 		st.NewVideojuego(),
 	}
 	arraySeries[0].Entregar()
 	arrayVideojuegos[1].Entregar()
-	fmt.Println("Series")
+	fmt.Println("Series Entregadas")
+	smay := 0
+	var vmay float32
+	cs := 0
+	cv := 0
 	for i, e := range arraySeries {
-		if arraySeries[i].Entregado {
+		if e.NTemporadas > smay {
+			smay = e.NTemporadas
+		}
+		if e.Entregado {
+			fmt.Println(i + 1)
 			fmt.Println(e)
 		}
 	}
-	fmt.Println("Videojuegos")
+	fmt.Println("\nSeries con más temporadas")
+	for _, e := range arraySeries {
+		if e.NTemporadas == smay {
+			cs += 1
+			fmt.Println(cs)
+			fmt.Println(e)
+		}
+	}
+	fmt.Println("\nVideojuegos Entregados")
 	for i, e := range arrayVideojuegos {
-		if arrayVideojuegos[i].Entregado {
+		if e.HorasEstimadas > vmay {
+			vmay = e.HorasEstimadas
+		}
+		if e.Entregado {
+			fmt.Println(i + 1)
+			fmt.Println(e)
+		}
+	}
+	fmt.Println("Videojuegos con mayor número de horas estimadas")
+	for _, e := range arrayVideojuegos {
+		if e.HorasEstimadas == vmay {
+			cv += 1
+			fmt.Println(cv)
 			fmt.Println(e)
 		}
 	}
