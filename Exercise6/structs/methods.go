59cbf34d0147e67b4d44569efd834f91ba707a5a
Christian <3003html@gmail.com>
2022-07-03 21:03:16 -0500

    getters created

--- Exercise6/structs/methods.go
+++ Exercise6/structs/methods.go
@@ -2,5 +2,14 @@ package structs
 
 // Getters
 func (l *Libro) getISBN() string {
-
+	return l.ISBN
+}
+func (l *Libro) getTitulo() string {
+	return l.Titulo
+}
+func (l *Libro) getAutor() string {
+	return l.Autor
+}
+func (l *Libro) getNPaginas() int {
+	return l.NPag
 }
