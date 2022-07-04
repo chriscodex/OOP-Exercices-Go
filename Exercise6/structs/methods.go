19872e54832fd6110e923f6ea97524c6caec7960
Christian <3003html@gmail.com>
2022-07-03 21:05:24 -0500

    setters created

--- Exercise6/structs/methods.go
+++ Exercise6/structs/methods.go
@@ -1,15 +1,29 @@
 package structs
 
 // Getters
-func (l *Libro) getISBN() string {
+func (l *Libro) GetISBN() string {
 	return l.ISBN
 }
-func (l *Libro) getTitulo() string {
+func (l *Libro) GetTitulo() string {
 	return l.Titulo
 }
-func (l *Libro) getAutor() string {
+func (l *Libro) GetAutor() string {
 	return l.Autor
 }
-func (l *Libro) getNPaginas() int {
+func (l *Libro) GetNPaginas() int {
 	return l.NPag
+}
+
+// Setters
+func (l *Libro) SetISBN(i string) {
+	l.ISBN = i
+}
+func (l *Libro) SetTitulo(t string) {
+	l.Titulo = t
+}
+func (l *Libro) SetAutor(a string) {
+	l.Autor = a
+}
+func (l *Libro) SetNPaginas(n int) {
+	l.NPag = n
 }
