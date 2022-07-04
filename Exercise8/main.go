f993241947c332ef9c83e1e1fb55c6d98f041171
Christian <3003html@gmail.com>
2022-07-04 03:10:52 -0500

    e8: constructor of new profesor created

--- Exercise8/main.go
+++ Exercise8/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	st "e8/structs"
+	"fmt"
+)
+
+func main() {
+	// est1 := st.NewEstudiante("Christian",24,"M",10)
+	// est2 := st.NewEstudiante("Eduardo",25,"M",8)
+	// est3 := st.NewEstudiante("Karolina",28,"F",8)
+	// est4 := st.NewEstudiante("Jesus",20,"M",10)
+	prof1 := st.NewProfesor("Juan", 44, "M", "matematicas")
+	fmt.Println(prof1)
+
+}
